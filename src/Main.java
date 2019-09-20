21261f09cc7e4155c8efeaf32e2a913ea41b0f02
ramilya <ramilya.kuramshina@gmail.com>
2019-09-21 00:13:53 +0300

    some changes added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        float expense=8;
-        float volume=50;
-        float distance=((volume/expense)*100);
+        float expense = 8;
+        float volume = 50;
+        float distance = ((volume / expense) * 100);
         System.out.println(distance);
     }
 }
